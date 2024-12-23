effae1e2578332ee9256078f47bfcbec4843cc0d
kimiaghrr <kimiaghorbani82@gmail.com>
2024-12-23 16:27:47 +0300

    changes

--- kd.py
+++ kd.py
@@ -12,7 +12,7 @@ def guessing_game():
             attempts += 1
             
             if guess < number_to_guess:
-                print("Too low!please again")
+                print("Too low!please try again. ")
             elif guess > number_to_guess:
                 print("Too high! Try again.")
             else:
